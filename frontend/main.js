534647d20e760609ca30b120e8f680a07967604e
Silvan Zurbruegg <silvan@liip.ch>
2011-01-10 15:49:48 +0100

    display results

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,18 @@
     var populateResults;
 
     populateResults = function (data) {
-        console.log(data);
+        var res = $('#results');
+        var ul = $('<ul/>'); 
+         
+        $.each(data, function(k, v) {
+            var t = '<li><h2>'+v.title+'</h2>'
+                  + '<div>'+v.text+'</div>' 
+                  + '<div>'+v.program+' &#187; '+v.container+'</div>'
+                  + '</li>'; 
+            console.log(t);
+            ul.append($(t));
+        });
+        res.html(ul);
     };
 
     $('#search').submit(function (e) {
